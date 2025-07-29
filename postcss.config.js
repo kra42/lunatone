0b27eb80339f24875fc0c93e0bf1863e1f786488
Jack <jackren36@gmail.com>
2025-07-28 23:25:11 -0700

    wip

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// This file uses the CommonJS format (`module.exports`) to ensure
+// compatibility with the Next.js build process.
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
