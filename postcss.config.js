c72785307415e7cb8d1d9ac2e4acdba9b50027bc
Jack <jackren36@gmail.com>
2025-07-30 19:48:34 -0700

    fixed tailwind styling not showing up

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,5 @@
-// This file uses the CommonJS format (`module.exports`) and the plugin name
-// that your build tool requires when it throws an error.
-
 module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
-    autoprefixer: {},
   },
 };
